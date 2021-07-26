27127b0800803e58fc88ee8b1eb540ebd348272b
Mubarak Imam <mimam@nibss-plc.com.ng>
2021-07-26 18:55:57 +0100

    deleted old front-end

--- nestpoc_ui/e2e/integration/app.spec.ts
+++
@@ -1,9 +0,0 @@
-import { getGreeting, navigateTo } from '../support/po';
-
-describe('Home Page', () => {
-  beforeEach(() => navigateTo('/'));
-
-  it('should have welcome message', () => {
-    getGreeting().contains('Welcome to nest-ui!');
-  });
-});
--- nestpoc_ui/e2e/support/po.ts
+++
@@ -1,3 +0,0 @@
-export const navigateTo = url => cy.visit(url);
-
-export const getGreeting = () => cy.get('app-root h1');
